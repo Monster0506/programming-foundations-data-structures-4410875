94bdc60b3996ba3166ff8fcb8a4151434838c65e
Monster0506 <tjraklovits@gmail.com>
2025-04-25 23:18:04 -0400

    Add 04_05b

--- 04/04_05/04_05b/main.py
+++ 04/04_05/04_05b/main.py
@@ -1,7 +1,9 @@
 def has_unique_characters(data):
-    return False
 
-print(has_unique_characters('sample'))
-print(has_unique_characters('hello world'))
-print(has_unique_characters('linkedin'))
-print(has_unique_characters('python'))
+    return len(set(data)) == len(data)
+
+
+print(has_unique_characters("sample"))
+print(has_unique_characters("hello world"))
+print(has_unique_characters("linkedin"))
+print(has_unique_characters("python"))
