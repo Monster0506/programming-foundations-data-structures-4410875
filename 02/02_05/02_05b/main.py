663b16aefb22d6c73c7f8cc71843fb5f5bbde6ea
Monster0506 <tjraklovits@gmail.com>
2025-04-25 22:33:01 -0400

    Add 02_05b

--- 02/02_05/02_05b/main.py
+++ 02/02_05/02_05b/main.py
@@ -2,5 +2,10 @@ seating_chart = [
     ["Sarah", "CLaire", "Ben", "Taylor", "Eva"],
     ["Frankie", "George", "Lindsey", "Izzy", "Jack"],
     ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
-    ["Chad", "April", "Matt", "Thomas", "Penny"]
+    ["Chad", "April", "Matt", "Thomas", "Penny"],
 ]
+
+
+for i, row in enumerate(seating_chart):
+    for j, student_name in enumerate(row):
+        print(f"{student_name} is in row {i+1}, seat {j+1}")
