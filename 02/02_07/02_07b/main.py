9f7e10fbab932dc2a5978a45f3c84d6fe84d82c1
Monster0506 <tjraklovits@gmail.com>
2025-04-25 22:35:27 -0400

    Add 02_07b

--- 02/02_07/02_07b/main.py
+++ 02/02_07/02_07b/main.py
@@ -1,1 +1,17 @@
 # Linear Search
+
+
+my_list = [8, 5, 0, 3, 9, 7]
+ITEM = 7
+
+
+def search(item, lst):
+    for element in lst:
+        if element == item:
+            return True
+    return False
+
+
+print(search(ITEM, my_list))
+
+ITEM_INDEX = my_list.index(ITEM)
