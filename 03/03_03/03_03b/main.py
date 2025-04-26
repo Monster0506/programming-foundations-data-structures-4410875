197e1d1abc19b0dcc667d9851255e0ba89858c1d
Monster0506 <tjraklovits@gmail.com>
2025-04-25 22:48:46 -0400

    Add 03_03b

--- 03/03_03/03_03b/main.py
+++ 03/03_03/03_03b/main.py
@@ -5,5 +5,15 @@ user_preferences = {
     "currency": "USD",
     "enable_location": False,
     "volume_level": 80,
-    "date_format": "MM/DD/YYYY"
+    "date_format": "MM/DD/YYYY",
 }
+
+
+user_preferences["language"] = "Spanish"
+user_preferences["volume_level"] = 50
+user_preferences["highlight_color"] = "Yellow"
+
+del user_preferences["currency"]
+user_preferences.pop("date_format")
+removed_item = user_preferences.pop("enable_location", "N/A")
+print(user_preferences)
