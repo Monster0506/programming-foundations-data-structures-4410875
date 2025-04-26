f7fb8f69719bcec08d5b25452dc4426d566a9932
Monster0506 <tjraklovits@gmail.com>
2025-04-25 23:09:39 -0400

    Add 04_02b

---
+++ 04/04_02/04_02b/main.py
@@ -0,0 +1,15 @@
+primary_colors = set(["red", "blue", "yellow"])
+color = "green"
+
+if color in primary_colors:
+    print(color + " is a primary color")
+else:
+    print(color + " is not a primary color")
+
+
+letters = set(["a", "b"])
+letters.add("c")
+letters.add("c")
+letters.add("c")
+letters.add("c")
+print(letters)
