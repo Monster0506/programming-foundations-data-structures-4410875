876dc88d18316cc7c731fb25736f7e7f780d07d9
Monster0506 <tjraklovits@gmail.com>
2025-04-25 22:42:41 -0400

    Add 02_09b

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,23 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) < 2:
+        return None
+
+    return sorted(my_list)[1]
+
+
+def find_second_smallest_v2(my_list):
+    if len(my_list) < 2:
+        return None
+    smallest = float("inf")
+    smallest_2 = float("inf")
+    for num in my_list:
+        smallest_2 = smallest
+        if num < smallest:
+            num = smallest
+        elif num < smallest_2:
+            smallest_2 = num
+    return smallest_2
+
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
