929e9008787b5817d6145084cd927df27159ddcf
Monster0506 <tjraklovits@gmail.com>
2025-04-25 23:41:19 -0400

    Add 06_03b

--- 06/06_03/06_03b/main.py
+++ 06/06_03/06_03b/main.py
@@ -1,1 +1,12 @@
 from collections import deque
+
+history_stack = deque()
+
+history_stack.append("https://google.com")
+history_stack.append("https://example.com")
+history_stack.append("https://github.com")
+
+
+print(history_stack[-1])
+print(history_stack.pop())
+
