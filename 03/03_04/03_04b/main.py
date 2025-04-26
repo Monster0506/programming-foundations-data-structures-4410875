534d6fe994a15fee3990b193911e33ff8df3b5a8
Monster0506 <tjraklovits@gmail.com>
2025-04-25 23:02:45 -0400

    Add 03_04b

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -3,12 +3,16 @@ user_preferences = {
     "language": "English",
     "notifications": None,
     "currency": "USD",
-    "theme": None
+    "theme": None,
 }
 
 
 def update_preferences(user_pref):
-    return {}
+    result = {}
+    for key, val in user_pref.items():
+        if val is not None:
+            result[key] = val
+    return result
 
 
 print(update_preferences(user_preferences))
